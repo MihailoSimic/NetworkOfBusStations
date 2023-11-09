f35f73e369b973ee3ea1815c75f1b158f1b1821e
MihailoSimic <127047249+MihailoSimic@users.noreply.github.com>
2023-11-09 16:54:10 +0100

    Add files via upload

---
+++ source.cpp
@@ -0,0 +1,15 @@
+#include "Network.h"
+#include "Interface.h"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <crtdbg.h>
+using namespace std;
+
+
+int main() {
+	
+	Interfejs* a = new Interfejs;
+	a->pokreniInterfejs();
+	
+}
